7c2db0ed91aa5eeafad338045c6efdc99f44f269
Nivalcir <nivalcir.souza@hotmail.com>
2026-03-21 17:39:06 -0300

    Fix: Remover import não usado formatDateTimeLocal em RegistrarViagem.js

--- src/components/RegistrarViagem.js
+++ src/components/RegistrarViagem.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
-import { formatDateTimeLocal } from '../utils/dateUtils';
 import { API_URL } from '../config';
 
 function RegistrarViagem({ veiculos, onSuccess }) {
